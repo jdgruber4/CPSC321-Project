f42ad1524b0ac7f08ba8879a5c71c7dae7220ca7
JD-Gruber <54658519+JD-Gruber@users.noreply.github.com>
2020-11-22 19:07:38 -0800

    p1 UI

---
+++ stat-view/src/App.js
@@ -0,0 +1,56 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+    <header class="header-banner">
+  <div class="container-width" id="iu9f">
+    <div class="clearfix">
+    </div>
+    <div class="lead-title" id="ivrvk">Stat View
+      <br/>
+    </div>
+  </div>
+</header>
+<section class="flex-sect" id="iwbf2">
+  <div class="container-width">
+    <div class="flex-title" id="if3qt">Select which category of stats you would like to see:
+      <br/>
+    </div>
+    <div class="cards">
+      <div class="card">
+        <div class="card-header">
+        </div>
+        <div class="card-body" id="i3sdc">
+          <div class="card-title" id="ij7cl">Players
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header ch2">
+        </div>
+        <div class="card-body">
+          <div class="card-title" id="ipvfl">Teams
+          </div>
+
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header ch3">
+        </div>
+        <div class="card-body" id="itbpi">
+          <div class="card-title" id="in04e">Coaches
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+  );
+}
+
+export default App;
