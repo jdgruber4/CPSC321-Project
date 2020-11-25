9b9305d981f53341b82e42f3d216ac1178d840ca
JD-Gruber <54658519+JD-Gruber@users.noreply.github.com>
2020-11-25 13:11:17 -0800

    add stats page

---
+++ stat-view/src/stat.js
@@ -0,0 +1,70 @@
+<div>
+<div class="row">
+</div>
+<div class="row" id="i0ya4">
+  <div class="cell" id="iz6gh">
+    <div id="if0si">Insert your text here
+    </div>
+  </div>
+  <div class="cell" id="imc18">
+    <div class="row" id="irijq">
+      <div class="cell" id="ikw8a">
+        <form class="form">
+          <div id="ixiqk">Select players
+            <br/>
+          </div>
+          <select class="select" id="iwwaw"><option value="">- Select option -</option><option value="1">Option 1</option></select>
+          <div class="form-group">
+          </div>
+          <div class="form-group">
+          </div>
+          <div class="form-group">
+          </div>
+        </form>
+      </div>
+      <div class="cell">
+        <form class="form">
+          <div id="ioxgj">Select stat
+            <br/>
+          </div>
+          <select class="select"><option value="">- Select option -</option><option value="1">Option 1</option></select>
+          <div class="form-group">
+          </div>
+          <div class="form-group">
+          </div>
+          <div class="form-group">
+          </div>
+        </form>
+      </div>
+      <div class="cell">
+        <form class="form" id="i3zxj">
+          <div id="ic50l">Select Season
+            <br/>
+          </div>
+          <select name="Select a Season" class="select" id="im2kl"><option value="">- Select option -</option><option value="">2018</option><option value="">2019</option><option value="">2020</option><option value="">All</option></select>
+          <div class="form-group">
+          </div>
+          <div class="form-group">
+          </div>
+          <div class="form-group">
+          </div>
+          <div class="form-group">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row" id="ix6bg">
+  <div class="cell" id="idwfg">
+    <div id="ib7sw">Player table
+      <br/>
+    </div>
+  </div>
+  <div class="cell" id="irlov">
+    <div id="iluk1">Stat table
+      <br/>
+    </div>
+  </div>
+</div>
+</div>
